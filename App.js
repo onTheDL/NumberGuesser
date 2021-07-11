ecf29b054fc93b9d4167229164de19e2beae2011
Daniel Laserna <daniel.laserna@protonmail.com>
2021-07-11 16:11:09 -0400

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Header from './components/Header'
+import StartScreen from './screens/StartScreen'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <Header title="Guess a Number" />
+      <StartScreen />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
+  }
 });
---
+++ components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+export default function Header({title}) {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.headerTitle}>{title}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  header: {
+    width: '100%',
+    height: 90,
+    backgroundColor: '#f7287b',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerTitle: {
+    color: 'black',
+    fontSize: 18,
+  },
+})
