83144c08eeb0dd6a1a2578d580102ae42fd803a8
Daniel Laserna <daniel.laserna@protonmail.com>
2021-07-11 16:39:28 -0400

    Add layout styling

--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -1,19 +1,43 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React from "react";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 
 export default function StartScreen() {
   return (
     <View style={styles.screen}>
-      <Text>The Game Screen</Text>
+      <Text style={styles.title}>Start a New Game</Text>
+      <View style={styles.inputContainer}>
+        <Text>Select a Number</Text>
+        <TextInput />
+        <View style={styles.btnBox}>
+          <Button title="Reset" onPress={() => {}} />
+          <Button title="Confirm" onPress={() => {}} />
+        </View>
+      </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 10,
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-  }
-})
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: '80%',
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  btnBox: {
+    flexDirection: "row",
+    width: '100%',
+    justifyContent: 'space-around',
+    paddingHorizontal: 15,
+  },
+  
+});
