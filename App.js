dec9f107e856495396f7d6e8cccf2441d45f56f1
Daniel Laserna <daniel.laserna@protonmail.com>
2021-07-13 19:07:27 -0400

    Add custom fonts

--- App.js
+++ App.js
@@ -1,18 +1,44 @@
 import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
+
 import Header from "./components/Header";
 import StartScreen from "./screens/StartScreen";
 import GameScreen from "./screens/GameScreen";
 import GameOverScreen from "./screens/GameOverScreen";
 
+// const fetchFonts = () => {
+//   return Font.loadAsync({
+//     "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+//     "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+//   });
+// };
+
 export default function App() {
   const [userNum, setUserNum] = useState();
   const [guessRounds, setGuessRounds] = useState(0);
+  // const [dataLoaded, setDataLoaded] = useState(false);
+
+  // if (!dataLoaded) {
+  //   return (
+  //     <AppLoading
+  //       startAsync={fetchFonts}
+  //       onFinish={() => setDataLoaded(true)}
+  //       onError={(err) => console.log(err)}
+  //     />
+  //   );
+  // }
+
+  let [fontsLoaded] = useFonts({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
 
   const handleRestartGame = () => {
-    setUserNum(null)
-    setGuessRounds(0)
-  }
+    setUserNum(null);
+    setGuessRounds(0);
+  };
 
   const handleStartGame = (selectedNum) => {
     setUserNum(selectedNum);
@@ -25,18 +51,27 @@ export default function App() {
   let content = <StartScreen onStart={handleStartGame} />;
 
   if (userNum && guessRounds <= 0) {
-    content = <GameScreen userChoice={userNum} onGameOver={handleGameOver} />
+    content = <GameScreen userChoice={userNum} onGameOver={handleGameOver} />;
   } else if (guessRounds > 0) {
-    content = <GameOverScreen guessRounds={guessRounds} userNumber={userNum} onRestart={handleRestartGame} />
+    content = (
+      <GameOverScreen
+        guessRounds={guessRounds}
+        userNumber={userNum}
+        onRestart={handleRestartGame}
+      />
+    );
   }
 
-
-  return (
-    <View style={styles.screen}>
-      <Header title="Guess a Number" />
-      {content}
-    </View>
-  );
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <View style={styles.screen}>
+        <Header title="Guess a Number" />
+        {content}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
